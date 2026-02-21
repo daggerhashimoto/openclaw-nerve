c3de033c4a5e12f0a6dc957c38039be5fb97870a
Kora <korathemaiden@gmail.com>
2026-02-21 23:47:11 +0000

    chore(voice): Remove VoiceProvidersSettings from exports

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -1,5 +1,4 @@
 export { SettingsDrawer } from './SettingsDrawer';
 export { ConnectionSettings } from './ConnectionSettings';
 export { AudioSettings } from './AudioSettings';
-export { VoiceProvidersSettings } from './VoiceProvidersSettings';
 export { AppearanceSettings } from './AppearanceSettings';
